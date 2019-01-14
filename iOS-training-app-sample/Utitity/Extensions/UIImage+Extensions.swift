0e3aab26b23543350864d961def36896f0a09bd3
kawashima41 <i.kawashima41@gmail.com>
2019-01-14 14:26:00 +0900

    create extensions

---
+++ iOS-training-app-sample/Utitity/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UIImage+Extensions.swift
+//  iOS-training-app-sample
+//
+//  Created by Iichiro Kawashima on 2019/01/07.
+//  Copyright © 2019 Iichiro Kawashima. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    final func applyImage(with url: URL) {
+
+        self.kf.setImage(with: url) { [weak self] image, error, _, _ in
+            if let _ = error, let image = image {
+                self?.image = image
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+}
