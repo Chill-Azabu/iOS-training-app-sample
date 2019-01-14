ff88b1e144dc1de37a2ae6028b2bcbfd841fc8ff
kawashima41 <i.kawashima41@gmail.com>
2019-01-14 14:33:50 +0900

    create tabbar

---
+++ iOS-training-app-sample/Presentation/TabBarViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TabBarViewController.swift
+//  iOS-training-app-sample
+//
+//  Created by Iichiro Kawashima on 2018/12/23.
+//  Copyright © 2018 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let homeView = HomeViewController.createInstance()
+        homeView.tabBarItem = UITabBarItem(title: "home", image:  UIImage(named: "tab_icon_seelcted"), tag: 1)
+        let homeBar = UINavigationController(rootViewController: homeView)
+
+        let logoutView = LogoutViewController.createInstance()
+        logoutView.tabBarItem = UITabBarItem(title: "logout", image:  UIImage(named: "tab_icon_seelcted"), tag: 2)
+        let logoutBar = UINavigationController(rootViewController: logoutView)
+
+        setViewControllers([homeBar, logoutBar], animated: false)
+    }
+}
+
+extension TabBarViewController {
+    static func createinstance() -> TabBarViewController {
+        let instance = TabBarViewController()
+        return instance
+    }
+}
